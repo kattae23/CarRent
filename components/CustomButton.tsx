a69590d77b3a2384e76691e34dc8bdc9ece1da1f
kattae23 <kattae.kattae@gmail.com>
2023-06-15 17:19:18 -0400

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { FC } from 'react'
+import { CustomButtonProps } from '@/types'
+import Image from 'next/image'
+
+const CustomButton: FC<CustomButtonProps> = ({
+  title,
+  containerStyles,
+  handleClick,
+  btnType = 'button',
+  textStyles,
+  rightIcon
+}) => {
+  return (
+    <button
+      disabled={false}
+      type={btnType}
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className={`flex-1 ${textStyles}`}>
+        {title}
+      </span>
+      {rightIcon && (
+        <div className='relative w-6 h-6'>
+          <Image
+            src={rightIcon}
+            alt='right icon'
+            fill
+            className='object-contain'
+          />
+        </div>
+      )}
+    </button>
+  )
+}
+
+export default CustomButton
---
+++ types/index.ts
@@ -0,0 +1,72 @@
+import { Dispatch, MouseEventHandler, SetStateAction } from 'react'
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export type CarState = CarProps[] & { message?: string };
+
+export interface SearchBarProps {
+  setManuFacturer: (manufacturer: string) => void;
+  setModel: (model: string) => void;
+}
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
+}
+
+export interface CustomButtonProps {
+  isDisabled?: boolean;
+  btnType?: 'button' | 'submit';
+  containerStyles?: string;
+  textStyles?: string;
+  title: string;
+  rightIcon?: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  options: OptionProps[];
+  setFilter: Dispatch<SetStateAction<number>> | Dispatch<SetStateAction<string>>
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+  setLimit: Dispatch<SetStateAction<number>>
+}
+
+export interface SearchManuFacturerProps {
+  selected: string;
+  setSelected: (selected: string) => void;
+}
